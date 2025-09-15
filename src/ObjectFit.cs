0a9f7d4012911e351e3a4109ce32e7f3626d1693
Jake Soenneker <jake@soenneker.com>
2025-09-15 16:55:31 -0500

    Initial commit

---
+++ src/ObjectFit.cs
@@ -0,0 +1,19 @@
+﻿using Intellenum;
+using Soenneker.Quark.Enums.GlobalKeywords;
+
+namespace Soenneker.Quark.Enums.ObjectFits;
+
+/// <summary>
+/// Typed values for the CSS object-fit property in .NET enumeration form.
+/// </summary>
+[Intellenum<string>]
+public sealed partial class ObjectFit
+{
+    public static readonly ObjectFit Contain = new("contain");
+    public static readonly ObjectFit Cover = new("cover");
+    public static readonly ObjectFit Fill = new("fill");
+    public static readonly ObjectFit ScaleDown = new("scale-down");
+    public static readonly ObjectFit None = new("none");
+
+    public static implicit operator ObjectFit(GlobalKeyword style) => new(style.Value);
+}
---
+++ test/Soenneker.Quark.Enums.ObjectFits.Tests/ObjectFitTests.cs
@@ -0,0 +1,13 @@
+﻿using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.Quark.Enums.ObjectFits.Tests;
+
+public sealed class ObjectFitTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
