fc045713df269e6f7b6876c1ab23c1e879d66ebe
Jake Soenneker <jake@soenneker.com>
2026-02-27 08:25:37 -0600

    Deps

--- src/ObjectFitKeyword.cs
+++ src/ObjectFitKeyword.cs
@@ -6,6 +6,7 @@ namespace Soenneker.Quark.Enums;
 /// Typed values for the CSS object-fit property in .NET enumeration form.
 /// </summary>
 [EnumValue<string>]
+[IncludeEnumValues(typeof(GlobalKeyword))]
 public sealed partial class ObjectFitKeyword
 {
     public static readonly ObjectFitKeyword Contain = new("contain");
@@ -13,6 +14,4 @@ public sealed partial class ObjectFitKeyword
     public static readonly ObjectFitKeyword Fill = new("fill");
     public static readonly ObjectFitKeyword ScaleDown = new("scale-down");
     public static readonly ObjectFitKeyword None = new("none");
-
-    public static implicit operator ObjectFitKeyword(GlobalKeyword style) => new(style.Value);
 }
