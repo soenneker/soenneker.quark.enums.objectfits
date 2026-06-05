32025a6885da1da4af882862917837f81183c0fa
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:37:15 -0500

    xml commenting

--- src/Soenneker.Quark.Enums.ObjectFits/ObjectFitKeyword.cs
+++ src/Soenneker.Quark.Enums.ObjectFits/ObjectFitKeyword.cs
@@ -9,9 +9,24 @@ namespace Soenneker.Quark;
 [IncludeEnumValues(typeof(GlobalKeyword))]
 public sealed partial class ObjectFitKeyword
 {
+    /// <summary>
+    /// The contain.
+    /// </summary>
     public static readonly ObjectFitKeyword Contain = new("contain");
+    /// <summary>
+    /// The cover.
+    /// </summary>
     public static readonly ObjectFitKeyword Cover = new("cover");
+    /// <summary>
+    /// The fill.
+    /// </summary>
     public static readonly ObjectFitKeyword Fill = new("fill");
+    /// <summary>
+    /// The scale down.
+    /// </summary>
     public static readonly ObjectFitKeyword ScaleDown = new("scale-down");
+    /// <summary>
+    /// The none.
+    /// </summary>
     public static readonly ObjectFitKeyword None = new("none");
 }
